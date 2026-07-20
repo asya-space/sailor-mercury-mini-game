1cacd64aae1b5d9a92096dc0ad95f0b22488d9a1
Asya Lebedeva <star@asyaspace.com>
2026-07-20 18:45:58 +0700

    build: add seperate script with constants.js

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+import { HEIGHT } from './utils/canvas.js';
+// canvas parameters
+export const LEVEL_WIDTH = 5500;
+export const GROUND_Y = HEIGHT - 50;
+export const  GRAVITY = 0.6;
+export const  JUMP_FORCE = -13; // ???
+export const  MOVE_SPEED = 4.5; // ???
