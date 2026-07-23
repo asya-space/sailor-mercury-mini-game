ebfca987e6588fbd8ad2475e73be2fbea994b043
Asya Lebedeva <star@asyaspace.com>
2026-07-24 01:47:19 +0700

    build: add objects for emenies

--- src/enemies/enemies.js
+++ src/enemies/enemies.js
@@ -7,11 +7,22 @@ export const enemies = [
         y: GROUND_Y - enemyTypes.purpleMonster.h,
         alive: true
     },
-
     {
         ...enemyTypes.cat,
         x: 800,
         y: GROUND_Y - enemyTypes.cat.h,
+        alive: true
+    },
+    {
+        ...enemyTypes.blueMonster,
+        x: 600,
+        y: GROUND_Y - enemyTypes.blueMonster.h,
+        alive: true
+    },
+    {
+        ...enemyTypes.redMonster,
+        x: 800,
+        y: GROUND_Y - enemyTypes.redMonster.h,
         alive: true
     }
 ]
