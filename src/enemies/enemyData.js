f63e05aee9fad6e13eff343744bbe1935a3a4b5d
Asya Lebedeva <star@asyaspace.com>
2026-07-24 01:48:34 +0700

    build: update object with types of enemies

--- src/enemies/enemyData.js
+++ src/enemies/enemyData.js
@@ -3,7 +3,7 @@ import { enemy1, enemy2, enemy3, enemy4 } from "../assets/images";
 export const enemyTypes = {
     purpleMonster: {
         image: enemy1,
-        w: 60,
+        w: 90,
         h: 60 * enemy1.ratio,
         hp: 50,
         speed: 2,
@@ -12,17 +12,17 @@ export const enemyTypes = {
 
     cat: {
         image: enemy2,
-        w: 48,
+        w: 50,
         h: 48 * enemy2.ratio,
         hp: 30,
         speed: 1,
         flying: true
     },
 
-    /*blueMonster: {
+    blueMonster: {
         image: enemy3,
         w: 50,
-        h: 50 * enemy3.ratio,
+        h: 70 * enemy3.ratio,
         hp: 30,
         speed: 2,
         flying: false
@@ -31,9 +31,9 @@ export const enemyTypes = {
     redMonster: {
         image: enemy4,
         w: 48,
-        h: 48 * enemy4.ratio,
+        h: 80 * enemy4.ratio,
         hp: 40,
         speed: 1,
-        flying: false
-    }*/
+        flying: true
+    }
 };
