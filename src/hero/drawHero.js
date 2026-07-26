5f776429af9b764cdfad143466f5002b01aab114
Asya Lebedeva <star@asyaspace.com>
2026-07-26 19:14:36 +0700

    build: add borders by size of the world

--- src/hero/drawHero.js
+++ src/hero/drawHero.js
@@ -1,7 +1,7 @@
 import { mercury } from '../assets/images.js';
 import { hero } from './hero.js';
 import { ctx } from '../utils/canvas.js';
-import { cameraX } from '../script.js';
+import { cameraX } from '../game/camera.js';
 
 export function drawHero() {
     ctx.drawImage(mercury, hero.x - cameraX, hero.y, hero.w, hero.h);
