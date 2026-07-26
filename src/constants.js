9016995d2361a7299193935b4b00c332d39e3f53
Asya Lebedeva <star@asyaspace.com>
2026-07-26 19:02:19 +0700

    fix: delete extra variable width world height

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
-import { HEIGHT } from './utils/canvas.js';
+import { WORLD_HEIGHT } from './utils/canvas.js';
 // canvas parameters
-export const LEVEL_WIDTH = 5500;
-export const GROUND_Y = HEIGHT - 50;
+export const GROUND_Y = WORLD_HEIGHT - 50;
 export const  GRAVITY = 0.6;
 export const  JUMP_FORCE = -13; // ???
 export const  MOVE_SPEED = 4.5; // ???
