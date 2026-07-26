f5d8809e7c40e64a76f638538e2e0df5763c82e9
Asya Lebedeva <star@asyaspace.com>
2026-07-26 19:16:11 +0700

    fix: delete extra imports and turn on last updates

--- src/script.js
+++ src/script.js
@@ -5,13 +5,9 @@ import { createGround } from './world/ground.js';
 import { createPlatforms } from './world/platforms.js';
 import { gameLoop } from './game/loop.js';
 import { createEnemies } from './enemies/enemies.js';
-import { enemyTypes } from './enemies/enemyData.js';
-import { platforms } from './world/platforms.js';
-import { GROUND_Y } from './constants.js';
 import './game/input.js';
 
 let gameRunning = false;
-export let cameraX = 0;
 // how to follow by state of the game
 
 preload(images, () => {
