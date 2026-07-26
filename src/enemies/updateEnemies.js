f65ff4f60594898cfbcdc2a6a62e262c9cf60a56
Asya Lebedeva <star@asyaspace.com>
2026-07-26 19:12:10 +0700

    build: add realizing of mirroring and moving enemies

--- src/enemies/updateEnemies.js
+++ src/enemies/updateEnemies.js
@@ -2,6 +2,18 @@ import { enemies } from "./enemies";
 
 export function updateEnemies() {
     enemies.forEach((enemy) => {
-        //
+        if (!enemy.alive) return;
+        enemy.x += enemy.velocityX * enemy.direction;
+
+        if (enemy.x <= enemy.leftLimit ) {
+            enemy.x = enemy.leftLimit;
+            enemy.direction = 1;
+            enemy.facing = 1;
+        }
+        if (enemy.x >= enemy.rightLimit) {
+            enemy.x = enemy.rightLimit;
+            enemy.direction = -1;
+            enemy.facing = -1;
+        }
     })
 }
