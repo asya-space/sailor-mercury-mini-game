512f206952147235788f848c3885ae7294e2d4a4
Asya Lebedeva <star@asyaspace.com>
2026-07-26 19:18:07 +0700

    fix: add correct import for camera

--- src/world/parallax.js
+++ src/world/parallax.js
@@ -1,8 +1,5 @@
-import { GROUND_Y } from '../constants.js';
-import { LEVEL_WIDTH } from '../constants.js';
-import { getPlatformX } from './platforms.js';
-import { WIDTH, ctx } from '../utils/canvas.js';
-import { cameraX } from '../script.js';
+import { ctx } from '../utils/canvas.js';
+import { cameraX } from '../game/camera.js';
 
 import { bg1, bg2, bg3, bg4, bg5  } from '../assets/images.js';
 
