9d07a7ef42d6938ce59f1c413dcd1c898aeb33bb
Asya Lebedeva <star@asyaspace.com>
2026-08-01 21:27:59 +0700

    build: add general variable for weapon object

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,10 @@ export const GROUND_Y = canvas.height - 45;
 export const  GRAVITY = 0.6;
 export const  JUMP_FORCE = -13; // ???
 export const  MOVE_SPEED = 4.5; // ???
+export const projectilesObj = {
+    x: 0,
+    y: 0,
+    speed: 0,
+    damage: 0,
+    radius: 8,
+} // bullets/balls
