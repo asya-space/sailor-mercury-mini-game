4b56fd395bacc34949d85e57a5ae6ffb10d8f80c
Asya Lebedeva <star@asyaspace.com>
2026-08-02 18:26:09 +0700

    fix: delete extra sources, update directories by their responsible

--- src/utils/canvas.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.querySelector('.canvas');
-const ctx = canvas.getContext('2d');
-
-const WORLD_WIDTH = 5500;
-const  WORLD_HEIGHT = 900;
-
-export { canvas, ctx, WORLD_WIDTH, WORLD_HEIGHT };
