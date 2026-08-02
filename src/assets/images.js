7af5926886d8eee4cbacd782a9c5cc66d4db9944
Asya Lebedeva <star@asyaspace.com>
2026-08-02 18:25:27 +0700

    build: add new img with weapon for redMonster

--- src/assets/images.js
+++ src/assets/images.js
@@ -30,7 +30,7 @@ mercury.src = '/characters/mercury.svg';
 
 // enemies
 export const enemy1 = new Image();
-enemy1.src = '/characters/monster-1.svg';
+enemy1.src = '/characters/monster-1.png';
 enemy1.ratio = 101 / 78;
 
 export const enemy2 = new Image();
@@ -38,11 +38,11 @@ enemy2.src = '/characters/monster-2.svg';
 enemy2.ratio  = 72 / 81;
 
 export const enemy3 = new Image();
-enemy3.src = '/characters/monster-3.svg';
+enemy3.src = '/characters/monster-3.png';
 enemy3.ratio = 339 / 577;
 
 export const enemy4 = new Image();
-enemy4.src = '/characters/monster-4.svg';
+enemy4.src = '/characters/monster-4.png';
 enemy4.ratio = 661 / 747;
 
 export const bossImg = new Image();
@@ -52,14 +52,24 @@ bossImg.ratio = 483 / 670;
 // weapon
 
 export const stick = new Image();
-stick.src = '/star-stick.png';
+stick.src = '/weapon/star-stick.png';
 stick.ratio = 208 / 62;
 
 export const starForPurple = new Image();
 starForPurple.src = '/weapon/purpleWeapon.png';
 starForPurple.ratio = 26 / 25;
 
-const images = [bg1, bg2, bg3, bg5, basic, stoneHand, mercury, enemy1, enemy2, enemy3, enemy4, bossImg, stick, starForPurple];
+export const redStar = new Image();
+redStar.src = '/weapon/redStar.png';
+redStar.ratio = 150 / 147;
+
+const images = [
+    bg1, bg2, bg3, bg5, 
+    basic, stoneHand, 
+    mercury, 
+    enemy1, enemy2, enemy3, enemy4, bossImg, 
+    stick, starForPurple, redStar
+];
 export { images };
 
 
