7319609e5ad296c86f333b40bcbdd88d463ee5a7
Asya Lebedeva <star@asyaspace.com>
2026-08-04 03:02:47 +0700

    build: separate and build script with general functions

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,26 +1,10 @@
-import { canvas, WORLD_HEIGHT } from './utils/canvas.js';
+import { canvas, WORLD_HEIGHT, ctx } from './utils/canvas.js';
+import { cameraX } from './game/camera.js';
 import { hero } from './hero/hero.js';
 // canvas parameters
 export const GROUND_Y = canvas.height - 45;
 export const GRAVITY = 0.6;
 export const JUMP_FORCE = -13;
 export const MOVE_SPEED = 4.5;
-
-// weapon data
-function getTargetHeroX() {
-    return hero.x + hero.w / 2;
-};
-
-function getTargetHeroY() {
-    return hero.y + hero.h / 2;
-}
-const weaponSpeed = 8;
-export const projectilesObj = {
-    x: 0,
-    y: 0,
-    speed: 0,
-    damage: 0,
-    radius: 8,
-} // bullets/balls
-
-export { getTargetHeroX, getTargetHeroY, weaponSpeed };
+export const weaponSpeed = 8;
+export const projectiles = [];
---
+++ src/js/functions.js
@@ -0,0 +1,89 @@
+import { hero } from './hero/hero.js';
+import { ctx } from './utils/canvas.js';
+import { cameraX } from './game/camera.js';
+
+// balls attacks
+function drawBalls(x, y, glowColor, color) {
+    //if (hero.cooldown <= 0) return;
+    const highlightX = x - cameraX - 2,
+          highlightY = y - 2,
+          highlightRadius = 2;
+    ctx.save();
+    ctx.shadowBlur = 18;
+    ctx.shadowColor = glowColor;
+
+    ctx.beginPath();
+    ctx.arc(x - cameraX, y, 6, 0, Math.PI * 2);
+
+    ctx.fillStyle = color;
+    ctx.fill();
+    // white light on the ball
+    ctx.beginPath();
+    ctx.arc(highlightX, highlightY, highlightRadius, 0, Math.PI * highlightRadius);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+    ctx.restore();
+};
+
+function updateAttacks(character, array) {
+    if (!character.isAttacking) {
+        array.forEach((item) => { 
+            item.x += item.speed;
+        });
+        return;
+    };
+
+    character.attackDuration--;
+
+    if (character.cooldown > 0) {
+        character.cooldown--;
+    };
+
+    if (character.cooldown <= 0) {
+        spawnAttacks(array, character);
+        character.cooldown = 10;
+    }
+
+    if (character.attackDuration <= 0) {
+            character.isAttacking = false;
+    };
+
+    array.forEach((item) => {
+            item.x += item.speed;
+    });
+};
+
+function updateBallProjectiles(balls) {
+    balls.forEach((ball) => {
+        ball.x += ball.speed;
+    });
+}
+
+function spawnAttacks(array, character) {
+    array.push({
+        x: (character.direction === 1) ? (character.x + character.w - 10) : (character.x + 10),
+        y: character.y + 25,
+        speed: 8 * character.direction,
+        damage: 10
+    });
+};
+
+// center of hero + stars-attacks (redMonster, purpleMonster)
+function getTargetHeroX() {
+    return hero.x + hero.w / 2;
+};
+
+function getTargetHeroY() {
+    return hero.y + hero.h / 2;
+};
+
+function weaponStartX(enemy) {
+    return (enemy.direction === 1) ? (enemy.x + enemy.weaponOffsetX)
+            : (enemy.x + enemy.w - enemy.weaponOffsetX);
+};
+
+function weaponStartY(enemy) {
+    return enemy.y + enemy.weaponOffsetY;
+};
+
+export { drawBalls, spawnAttacks, getTargetHeroX, getTargetHeroY, weaponStartX, weaponStartY, updateAttacks, updateBallProjectiles }
