1a6069ee4472731996c25e604c6c94aff26a5e17
Asya Lebedeva <star@asyaspace.com>
2026-08-04 03:14:42 +0700

    build: move weapon with special params (vx, vy, targetHeroX, targetHeroY, images for weapon, etc.) into special directory for correct responsible between functions for different types of weapons

---
+++ src/js/weapon/stars/drawWeapon.js
@@ -0,0 +1,23 @@
+import { drawBalls } from '../../functions.js';
+import { projectiles } from '../../constants.js';
+
+import { cameraX } from '../../game/camera.js';
+import { ctx } from '../../utils/canvas.js';
+import { enemyProjectiles } from './createWeapon.js';
+
+function drawWeapon(array) {
+    array.forEach((weapon) => {
+        const weaponW = 20;
+        const weaponH = weaponW * weapon.img.ratio;
+
+        ctx.drawImage(
+            weapon.img,
+            weapon.x - cameraX,
+            weapon.y,
+            weaponW,
+            weaponH
+        );
+    });
+};
+
+export { drawWeapon };
