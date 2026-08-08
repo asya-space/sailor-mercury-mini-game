79ea12e98ba14d99f3c3df8cc3d1ef46c0fbf70a
Asya Lebedeva <star@asyaspace.com>
2026-08-08 07:14:34 +0700

    build: add the first attacking for boss character, create-push-update-delete functions

--- src/js/boss/boss.js
+++ src/js/boss/boss.js
@@ -1,6 +1,13 @@
 import { ctx } from '../utils/canvas.js';
-import { bossImg } from '../../assets/images.js';
+import { bossImg, bossStar } from '../../assets/images.js';
 import { cameraX } from '../game/camera.js';
+
+import { weaponSpeed } from '../constants.js';
+import { updateEnemyProjectile } from '../weapon/stars/updateProjectiles.js';
+import { drawWeapon } from '../weapon/stars/drawWeapon.js';
+import { getTargetHeroX, getTargetHeroY /*weaponStartX, weaponStartY*/ } from '../functions.js';
+
+export const bossProjectiles = [];
 
 class Boss {
     constructor(x, y, h, area) {
@@ -11,11 +18,17 @@ class Boss {
         this.hp = 200;
         this.phase = 1;
         this.state = 'fly';
-        this.cooldown = 0;
         this.speed = 7;
         this.alive = true;
         this.area = area;
         this.target = this.getRandomTarget();
+        this.isAttacking = false;
+        this.cooldown = 0;
+        this.cooldownTimer = 0;
+        this.chargeTimer = 0;
+        this.isCharging = 0;
+        this.shotLeft = 0;
+        this.shotTimer = 0;
     }
 
     draw(ctx) {
@@ -30,7 +43,6 @@ class Boss {
     }
 
     move() {
-        
         if (this.x < this.target.x) {
             this.x = Math.min(this.x + this.speed, this.target.x);
         };
@@ -52,12 +64,53 @@ class Boss {
         };
     }
 
+    createProjectile() {
+        const angle = Math.atan2(getTargetHeroY() - this.y, getTargetHeroX() - this.x);
+        const projectile = {
+            img: bossStar,
+            x: this.x,
+            y: this.y,
+            speed: weaponSpeed,
+            damage: 20,
+            vx: Math.cos(angle) * weaponSpeed,
+            vy: Math.sin(angle) * weaponSpeed
+        };
+        bossProjectiles.push(projectile);
+        console.log('BOSS SHOOT', projectile);
+    }
+
+    attack() {
+        if (this.cooldownTimer > 0) {
+            this.cooldownTimer--;
+            return;
+        };
+
+        if (this.shotLeft > 0) {
+            if (this.shotTimer > 0) {
+                this.shotTimer--;
+                return;
+            };
+            this.createProjectile();
+            this.shotLeft--;
+            this.shotTimer = 10;
+            return;
+        };
+        this.shotLeft = 5;
+        this.shotTimer = 0;
+        this.cooldownTimer = 120;
+    }
+
+    updateProjectiles() {
+        updateEnemyProjectile(bossProjectiles);
+    }
+
     update() {
         if (!this.alive) return;
 
         this.move();
         this.checkTarget();
-
+        this.attack();
+        this.updateProjectiles();
     }
 }
 
