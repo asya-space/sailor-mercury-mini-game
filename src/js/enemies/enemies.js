e52a69f7a7b5ea8897c25377723a208fbfa9c36d
Asya Lebedeva <star@asyaspace.com>
2026-08-30 01:21:38 +0700

    refactor: add one function for creating array with enemies instead of a lot repeating functions for every enemy

--- src/js/enemies/enemies.js
+++ src/js/enemies/enemies.js
@@ -1,25 +1,25 @@
-import { platforms } from "../world/platforms";
-import { enemy1, enemy2, enemy3, enemy4 } from "../../assets/images";
-import { Enemy, AirEnemy, GroundMonster } from "./enemyData";
+import { platforms } from '../world/platforms.js';
+import { enemy1, enemy2, enemy3, enemy4 } from '../../assets/images.js';
+import { Enemy, AirEnemy, GroundMonster } from './enemyData.js';
 
 const enemies = [];
 
 function createEnemy() {
     const catMonsters = [
-        new Enemy(enemy2, 50, 48 * enemy2.ratio, 1480, 30, 1, 'catMonster', platforms[6]),
-        new Enemy(enemy2, 50, 48 * enemy2.ratio, 2350, 30, 1, 'catMonster', platforms[10]),
-        new Enemy(enemy2, 50, 48 * enemy2.ratio, 4500, 30, 1, 'catMonster', platforms[20])
+        new Enemy(enemy2, 50, 48 * enemy2.ratio, platforms[6].x, 30, 1, 'catMonster', platforms[6]),
+        new Enemy(enemy2, 50, 48 * enemy2.ratio, platforms[10].x, 30, 1, 'catMonster', platforms[10]),
+        new Enemy(enemy2, 50, 48 * enemy2.ratio, platforms[20].x, 30, 1, 'catMonster', platforms[20])
     ];
 
     const blueMonsters = [
-        new Enemy(enemy3, 50, 70 * enemy3.ratio, 2750, 30, 1, 'blueMonster', platforms[12]),
-        new Enemy(enemy3, 50, 70 * enemy3.ratio, 3600, 30, 1, 'blueMonster', platforms[16]),
-        new Enemy(enemy3, 50, 70 * enemy3.ratio, 4900, 30, 1, 'blueMonster', platforms[22])
+        new Enemy(enemy3, 50, 70 * enemy3.ratio, platforms[12].x, 30, 1, 'blueMonster', platforms[12]),
+        new Enemy(enemy3, 50, 70 * enemy3.ratio, platforms[16].x, 30, 1, 'blueMonster', platforms[16]),
+        new Enemy(enemy3, 50, 70 * enemy3.ratio, platforms[22].x, 30, 1, 'blueMonster', platforms[22])
     ];
 
     const airMonsters = [
-        new AirEnemy(enemy4, 48, 80 * enemy4.ratio, 1700, 40, 1, 'airMonster', platforms[2]),
-        new AirEnemy(enemy4, 48, 80 * enemy4.ratio, 3800, 40, 1, 'airMonster', platforms[18])
+        new AirEnemy(enemy4, 48, 80 * enemy4.ratio, platforms[2].x, 40, 1, 'airMonster', platforms[2]),
+        new AirEnemy(enemy4, 48, 80 * enemy4.ratio, platforms[18].x, 40, 1, 'airMonster', platforms[18])
     ];
 
     const groundMonsters = [
@@ -28,8 +28,12 @@ function createEnemy() {
         new GroundMonster(enemy1, 100, 60 * enemy1.ratio, 3150, 50, 1, 'groundMonster', 3150, 3550),
         new GroundMonster(enemy1, 100, 60 * enemy1.ratio, 4550, 50, 1, 'groundMonster', 4550, 5000)
     ];
-
-    enemies.push(catMonsters, blueMonsters, airMonsters, groundMonsters);
+    enemies.push(
+        ...catMonsters, 
+        ...blueMonsters, 
+        ...airMonsters, 
+        ...groundMonsters
+    );
 };
 
-export { createEnemy, enemies}
+export { createEnemy, enemies }
