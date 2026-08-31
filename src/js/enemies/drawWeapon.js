0b28bb271d06390ba4082505be15225facecd5d3
Asya Lebedeva <star@asyaspace.com>
2026-08-31 17:52:58 +0700

    refactor: add draw() and update() functions for enemies separately and into new directory

---
+++ src/js/enemies/drawWeapon.js
@@ -0,0 +1,27 @@
+import { cameraX } from '../game/camera.js';
+import { ctx } from '../utils/canvas.js';
+import { drawBalls } from '../functions.js';
+
+// draw attacks for GroundMonster and AirMonster
+function drawWeapon(array) {
+    array.forEach((weapon) => {
+        const weaponW = 20;
+        const weaponH = weaponW * weapon.img.ratio;
+
+        ctx.drawImage(
+            weapon.img,
+            weapon.x - cameraX,
+            weapon.y,
+            weaponW,
+            weaponH
+        );
+    });
+};
+
+function drawMonstersBalls(projectiles) {
+    projectiles.forEach(projectile => {
+        drawBalls(projectile.x, projectile.y, projectile.color1, projectile.color2);
+    });
+}
+
+export { drawWeapon, drawMonstersBalls };
---
+++ src/js/enemies/updateProjectiles.js
@@ -0,0 +1,32 @@
+import { WORLD_WIDTH, canvas } from '../utils/canvas.js';
+ // for GroundMonster and AirMonster
+function updateEnemyProjectile(array) {
+    for (let i = array.length - 1; i >= 0; i--) {
+
+        const weapon = array[i];
+
+        weapon.x += weapon.vx;
+        weapon.y += weapon.vy;
+
+        if (
+            weapon.x < -100 ||
+            weapon.x > WORLD_WIDTH + 100 ||
+            weapon.y < -100 ||
+            weapon.y > canvas.height + 100
+        ) {
+            array.splice(i, 1);
+        }
+    }
+};
+
+function updateMonsterProjectiles(projectiles) {
+    for (let i = projectiles.length - 1; i >= 0; i--) {
+        const weapon = projectiles[i];
+        weapon.x += weapon.speed;
+            
+        if (weapon.x < -100 || weapon.x > WORLD_WIDTH + 100) {
+            projectiles.splice(i, 1);
+        };
+    };
+}
+export { updateEnemyProjectile, updateMonsterProjectiles };
